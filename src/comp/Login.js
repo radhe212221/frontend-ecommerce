a99232a2323557f602b4032a9b5dfef25c892cb2
RS <radhe212221@gmail.com>
2022-06-26 14:12:38 +0530

    added login signup

--- src/comp/Login.js
+++ src/comp/Login.js
@@ -1,7 +1,34 @@
-import React from 'react'
-
+import React, { useContext, useState } from "react";
+import { AppContext } from "../index";
+import axios from "../lib/axios";
 export default function Login() {
+  const { state, setState } = useContext(AppContext);
+  const [ob, setob] = useState({
+    email: "",
+    password: "",
+  });
+
+  const login=async ()=>{
+    let resp=await axios.post("/login",ob).then(res=>res.data)
+    console.log(resp)
+
+  }
   return (
-    <div>Login</div>
-  )
+    <div>
+      <h1>Login</h1>
+      <input
+        placeholder="email"
+        value={ob?.email}
+        onChange={({target}) => setob({ ...ob, email: target.value })}
+      />
+      
+      <input
+        placeholder="password"
+        value={ob?.password}
+        onChange={({target}) => setob({ ...ob, password: target.value })}
+      />
+
+      <button onClick={login}>login</button>
+    </div>
+  );
 }
--- src/comp/Signup.js
+++ src/comp/Signup.js
@@ -1,7 +1,53 @@
-import React from 'react'
-
+import React, { useContext, useState } from "react";
+import { AppContext } from "../index";
+import axios from "../lib/axios";
 export default function Signup() {
+  const { state, setState } = useContext(AppContext);
+  const [ob, setob] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+
+  const signup=async ()=>{
+    let resp=await axios.post("/signup",ob).then(res=>res.data)
+    if(resp?.status)
+    {
+      alert("signup done")
+    }
+    else{
+      alert(resp?.err)
+    }
+  }
   return (
-    <div>Signup</div>
-  )
+    <div>
+      <h1>Signup</h1>
+      <input
+        placeholder="name"
+        value={ob?.name}
+        onChange={({target}) => setob({ ...ob, name: target.value })}
+      />
+      
+      <input
+        placeholder="email"
+        value={ob?.email}
+        onChange={({target}) => setob({ ...ob, email: target.value })}
+      />
+      <input
+        placeholder="phone"
+        value={ob?.phone}
+        onChange={({target}) => setob({ ...ob, phone: target.value })}
+      />
+
+      
+      <input
+        placeholder="password"
+        value={ob?.password}
+        onChange={({target}) => setob({ ...ob, password: target.value })}
+      />
+
+      <button onClick={signup}>Signup</button>
+    </div>
+  );
 }
