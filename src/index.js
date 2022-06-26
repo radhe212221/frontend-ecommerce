cea5b4fd8caf62dc6ab61eea36cf5522c1a32d99
RS <radhe212221@gmail.com>
2022-06-26 13:17:13 +0530

    added basic context

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const AppContext = React.createContext();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Root = () => {
+  const [state, setState] = useState({
+    users: [],
+    products: [],
+    cart: [],
+    orders: [],
+    user: null,
+    loggedin: false,
+    tags: [],
+    filters: ["old", "new", "rating", "discount"],
+  });
+
+  const value = { state, setState };
+  return (
+    <AppContext.Provider value={value}>
+      <App />
+    </AppContext.Provider>
+  );
+};
+
+const el = document.getElementById("root");
+const root = createRoot(el);
+root.render(<Root />);
